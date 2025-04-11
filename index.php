b0828b5b751e173df2895a2440c5429327cfeca7
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 20:58:03 +0600

    doing

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
   <title>Document</title>
 </head>
 <body>
-  <h1 class="text-3xl bg-slate-500 text-white py-5 px-20 w-fit rounded-2xl hover:bg-sky-400 active:bg-sky-600 ">Working</h1>
 
-  <div class="w-80 h-80 bg-amber-100 my-4"></div>
 </body>
 </html>
