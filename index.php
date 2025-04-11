e0f95ebce1adabd770fb310c709330cdf35860e0
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 21:35:20 +0600

    doing

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
   <title>Document</title>
 </head>
 <body>
-
+<div class="flex justify-between px-20 items-center
+bg-slate-50  shadow-md 2xl:px-80 ">
+  <div class="text-4xl font-bold my-4">Abrar</div>
+  <nav>
+    <ul class="flex">
+      <li><a href="#"class="mx-5 2xl:mx-10 hover:text-blue-700">Blog</a></li>
+      <li><a href="#"class="mx-5 2xl:mx-10 hover:text-blue-700">Address</a></li>
+      <li><a href="#"class="mx-5 2xl:mx-10 hover:text-blue-700">Portfolio</a></li>
+      <li><a href="#"class="mx-5 2xl:mx-10 hover:text-blue-700">Home</a></li>
+    </ul>
+  </nav>
+  <button class="bg-blue-500 text-white px-5 py-2 rounded-sm text-md hover:bg-blue-600">Contact</button>
+</div>
 </body>
 </html>
