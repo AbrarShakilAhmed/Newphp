4bcd666b4a63870d38044641212d7b8e664f4427
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 15:52:39 +0600

    doing

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function createPerson(name, age, address) {
 }
 person1 = createPerson('abrar', 34, 'address')
 console.log(person1)
+
+function contructerFunctionPerson(name, age) {
+  ;(this.name = name), (this.age = age)
+}
+
+person3 = new contructerFunctionPerson('abver', 324)
+
+console.log(person3.age)
