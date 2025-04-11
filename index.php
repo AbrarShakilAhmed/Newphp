f0b5b72db42ed42f1d926a213468ce0f79fe778a
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 18:28:34 +0600

    doing

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-<script>
-  function createPerson(name, age, address) {
-  return {
-    name,
-    age: age,
-    address: address,
-  }
-}
-person1 = createPerson('Abrar', 54, 'address');
-console.log(person1);
-</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="app.css">
+  <title>Document</title>
+</head>
+<body>
+  <h1 class="text-3xl bg-slate-500 text-white py-5 px-20 w-fit rounded-2xl hover:bg-sky-400 active:bg-sky-600 ">Working</h1>
+
+  <div class="w-80 h-80 bg-amber-100 my-4"></div>
+</body>
+</html>
