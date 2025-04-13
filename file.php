588878b0b4957f3914aa7b664a55bdd68db9d94d
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-13 11:14:26 +0600

    doing

--- file.php
+++ file.php
@@ -1,9 +1,19 @@
 <?php
 if($_FILES["file"]["error"]==0){
-  print_r($_FILES);
-$targetDir = "./UploadPhoto/";
-$targetFile = $targetDir . basename($_FILES["file"]["name"]);
-
-move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile);
-
+move_uploaded_file($_FILES["file"]['tmp_name'],"./UploadPhoto/".basename($_FILES["file"]["name"]));
 }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <img src="<?php echo './UploadPhoto/'.basename($_FILES['file']['name'])?>"  alt="" width="400">
+
+</body>
+</html>
