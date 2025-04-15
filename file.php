005770cf9357b84f5481bafb68d5b25eaf5037ea
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-15 11:27:54 +0600

    doing

--- file.php
+++ file.php
@@ -3,6 +3,7 @@ if($_FILES["file"]["error"]==0){
 move_uploaded_file($_FILES["file"]['tmp_name'],"./UploadPhoto/".basename($_FILES["file"]["name"]));
 }
 
+setcookie('./UploadPhoto/'.basename($_FILES['file']['name']),"nameOfImg")
 
 ?>
 <!DOCTYPE html>
